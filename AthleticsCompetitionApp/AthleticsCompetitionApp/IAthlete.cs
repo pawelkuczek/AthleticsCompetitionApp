ca87368b39dc0e0beef4257ca0e163521bf6053d
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-08-05 01:13:07 +0200

    Create IAthlete interface

---
+++ AthleticsCompetitionApp/AthleticsCompetitionApp/IAthlete.cs
@@ -0,0 +1,18 @@
+﻿namespace AthleticsCompetitionApp
+{
+    public interface IAthlete
+    {
+        string Name { get; }
+        string Surname { get; }
+
+        public void Add100meterRunResult(double result);
+        public void Add100meterRunResult(string result);
+
+        public void AddLongJumpResult(double result);
+        public void AddLongJumpResult(string result);
+        public void AddShotPutResult(double result);
+
+        public void AddShotPutResult(string result);
+
+    }
+}
