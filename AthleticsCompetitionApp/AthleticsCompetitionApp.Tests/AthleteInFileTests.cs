cbaf88980768435da1e8093dc92b5550f70233a0
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-08-05 04:10:15 +0200

    Add static method to delete test file after test is done in AthleteInFileTests

--- AthleticsCompetitionApp/AthleticsCompetitionApp.Tests/AthleteInFileTests.cs
+++ AthleticsCompetitionApp/AthleticsCompetitionApp.Tests/AthleteInFileTests.cs
@@ -25,7 +25,7 @@
                 var lines = reader.ReadToEnd();
                 Assert.AreEqual("Wyniki zawodnika: Usain Bolt\r\nBieg na 100m: 9,88s\r\nSkok w dal: 8,92m\r\nPchnięcie kulą: 22,55m\r\nIlość punktów: 28 pkt\r\nKlasa zawodnika: A\r\n---------------------\r\n", lines);
             }
-
+            File.Delete(fileName);
         }
 
     }
