0423a13abc25289757a58800b13f82fd0be63ed8
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-08-05 04:04:51 +0200

    Create AthleteInFile class

---
+++ AthleticsCompetitionApp/AthleticsCompetitionApp/AthleteInFile.cs
@@ -0,0 +1,25 @@
+﻿namespace AthleticsCompetitionApp
+{
+    public class AthleteInFile : AthleteInMemory
+    {
+        private const string fileName = "athletes_results.txt";
+        public AthleteInFile(string name, string surname) : base(name, surname) { }
+    
+      public void SaveAthleteResultsToFile() { 
+
+            var athleteResults = this.GetAthleteResults();
+
+            using (var writer = File.AppendText(fileName))
+            {
+                writer.WriteLine($"Wyniki zawodnika: {this.Name} {this.Surname}");
+                writer.WriteLine($"Bieg na 100m: {athleteResults.Athlete100mRunResult}s");
+                writer.WriteLine($"Skok w dal: {athleteResults.AthleteLongJumpResult}m");
+                writer.WriteLine($"Pchnięcie kulą: {athleteResults.AthleteShotPutResult}m");
+                writer.WriteLine($"Ilość punktów: {athleteResults.Score} pkt");
+                writer.WriteLine($"Klasa zawodnika: {athleteResults.AthleteClassification}");
+                writer.WriteLine("---------------------");
+
+            }
+        } 
+    }
+}
--- AthleticsCompetitionApp/AthleticsCompetitionApp/Program.cs
+++ AthleticsCompetitionApp/AthleticsCompetitionApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AthleticsCompetitionApp;
+
+Console.WriteLine("Test programu Zawody Lekkoatletyczne");
+var athlete = new AthleteInFile("Usain", "Bolt");
+
+athlete.Add100meterRunResult(9.88);
+athlete.AddLongJumpResult(8.92);
+athlete.AddShotPutResult(22.55);
+
+athlete.SaveAthleteResultsToFile();
+
+using (var reader = File.OpenText("athletes_results.txt"))
+{
+    var lines = reader.ReadToEnd();
+    Console.WriteLine(lines);
+}
